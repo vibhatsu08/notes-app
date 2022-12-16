f0729f855d7264fde7e1d45094971530732fed4f
vibhatsu08 <mistryvedant17@gmail.com>
2022-12-16 17:21:31 +0530

    Exercise example for objects/

--- script.js
+++ script.js
@@ -1,17 +1,12 @@
-import Note from "./note.js";
-
-const batmanNote = new Note (
-    "batman movie",
-    "it was a great movie, about 3 hours long, wonderful cinematography by the director matt reeves."
-);
-console.log(batmanNote);
-
-const yesterdayNote = new Note (
-    "star wars movie",
-    "that too was a great movie, as always the older star wars are way better than the star wars movies at present."
-);
-
-const hello = {
-    prop name = 5;
+// Create an empty object user
+const user = {
 }
-console.log(yesterdayNote);
+// Add the property name with the value John
+user.name = "John";
+// Add the property surname with the value Smith
+user.surname = "Smith";
+// Change the value of the name to Pete
+user.name = "Pete";
+// Remove the property name from the object
+delete user.name;
+console.log(user);
